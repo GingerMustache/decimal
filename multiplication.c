865d1ee7dd263cefc8825494b5af0d78e19bc2a8
Ginger_mustache <artzaneg@gmail.com>
2023-05-06 14:31:04 +0300

    little rewrite s21_mul

--- multiplication.c
+++ multiplication.c
@@ -4,11 +4,13 @@
 int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal* result) {
   int output = CONVERSATION_OK;
 
-  int count_1 = 0, count_2 = 0;
-  int index = 0;
+  // int count_1 = 0, count_2 = 0;
+  // int index = 0;
   int sign_1 = s21_get_bit(&value_1, 127);
   int sign_2 = s21_get_bit(&value_2, 127);
   s21_set_dec_number_to_0(result);
+  int result_of_big_decimal = 0;
+  // int power_of_result = 0;
   // s21_decimal tmp = *result;
   // s21_decimal step = {0};
   // int power_of_result = 0;
@@ -26,12 +28,12 @@ int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal* result) {
   //   s21_normalize(&value_1, &value_2);  // нормализация
   // }
 
-  while (index != 63) {  // проверка что на что умножать будет
-    count_1 += s21_get_bit(&value_1, index);
-    count_2 += s21_get_bit(&value_2, index);
-    index++;
-  }
-  index = 0;
+  // while (index != 63) {  // проверка что на что умножать будет
+  //   count_1 += s21_get_bit(&value_1, index);
+  //   count_2 += s21_get_bit(&value_2, index);
+  //   index++;
+  // }
+  // index = 0;
 
   s21_big_decimal big_value_1 = {0};
   s21_big_decimal big_value_2 = {0};
@@ -39,12 +41,23 @@ int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal* result) {
   rewrite_decimal_to_big(&big_value_1, value_1);
   rewrite_decimal_to_big(&big_value_2, value_2);
 
-  // можно учесть умножение на 0 сразу
-  if (count_1 < count_2) {
-    s21_big_mul(big_value_1, big_value_2, &big_result, 1);
+  if (s21_is_equal_big(big_value_1, big_result) ||
+      s21_is_equal_big(big_value_2, big_result)) {
+    output = CONVERSATION_OK;
   } else {
-    s21_big_mul(big_value_2, big_value_1, &big_result, 1);
+    result_of_big_decimal =
+        s21_big_mul(big_value_1, big_value_2, &big_result, 1);
+    if (!result_of_big_decimal) {
+      rewrite_from_big_decimal_to_decimal(big_result, result);
+    } else if (result_of_big_decimal == 1) {
+      output = CONVERSATION_BIG;
+    } else if (result_of_big_decimal == 2) {
+      output = CONVERSATION_SMALL;
+    }
   }
+
+  // можно учесть умножение на 0 сразу
+
   // case 1, где меньше едииц на то и умножаем
   //   if (count_1 < count_2) {
   //     while (index != 96 && output == 0) {
@@ -98,25 +111,20 @@ int s21_big_mul(s21_big_decimal big_value_1, s21_big_decimal big_value_2,
   s21_big_decimal step = {0};
   s21_set_big_dec_number_to_0(result);
   s21_big_decimal tmp = *result;
-  // s21_big_decimal garbage = {0};
-  s21_big_decimal big_10 = {10, 0, 0, 0, 0, 0, 0};  // не удаляq
+  s21_big_decimal big_10 = {10, 0, 0, 0, 0, 0, 0};
   int count_1 = 0, count_2 = 0;
   int power_of_1 = s21_get_power_of_big_decimal(big_value_1);
   int power_of_2 = s21_get_power_of_big_decimal(big_value_2);
   int power_of_result = power_of_1 + power_of_2;
-  // power больше 1, когда кличество значаящих единиц в двух меожителя
-  // превосходи 95 бит
-  // int power = 0;
-  // int flg_div = 1;
 
   s21_big_decimal fractional = {0};
   s21_big_decimal value_unsigned_truncated = {0};
-  // if (func == 1) power = count_ones(big_value_1, big_value_2);
 
   if ((power_of_1 || power_of_2)) {
     s21_normalize_big(&big_value_1, &big_value_2);  // нормализация
     power_of_result += abs(power_of_1 - power_of_2);
   }
+
   while (index != 63) {  // проверка что на что умножать будет
     count_1 += s21_get_bit_big(&big_value_1, index);
     count_2 += s21_get_bit_big(&big_value_2, index);
@@ -124,6 +132,7 @@ int s21_big_mul(s21_big_decimal big_value_1, s21_big_decimal big_value_2,
   }
   index = 0;
 
+  // само умножение путем сдвига
   if (count_1 < count_2) {
     while (index != 192 && output) {
       if (s21_get_bit_big(&big_value_1, index)) {
@@ -145,43 +154,26 @@ int s21_big_mul(s21_big_decimal big_value_1, s21_big_decimal big_value_2,
       index++;
     }
   }
-  // if (sign_1 != sign_2) {        // постановка знака
-  //   s21_set_bit_1_big(result, 223);
-  // } else {
-  //   s21_set_bit_0_big(result, 223);
-  // }
 
   if (func == 1) {
-    // s21_print_big_decimal_number(&big_tmp);
-    // s21_truncate_zero_big(&tmp);
-
-    if (power_of_1 || power_of_2) {  // было &&
+    if (power_of_1 || power_of_2) {
       power_of_result -= s21_truncate_zero_big(&tmp);
-      s21_print_big_decimal_number(&tmp);
+      // s21_print_big_decimal_number(&tmp);
     }
-    s21_print_big_decimal_number(&tmp);
+    // s21_print_big_decimal_number(&tmp);
     int rewrite = check_big_decimal(tmp);
     if (rewrite == 3) {
       *result = tmp;
-      // нужна проверка на степень
-      // убираем нули
     } else if (power_of_result) {
-      while (power_of_result && rewrite != 3) {  // сменил ||, как в вычитании
-        // s21_set_power_of_big_decimal(&tmp, power_of_result);
+      while (power_of_result && rewrite != 3) {
         s21_div_big(tmp, big_10, &tmp);
-        // s21_div_decimal_by_10_big(&tmp, &garbage);
-        s21_print_big_decimal_number(&tmp);
+        // s21_print_big_decimal_number(&tmp);
         rewrite = check_big_decimal(tmp);
-        // if (rewrite != 3) {
-        // s21_round_big(tmp, &tmp);
-        // замена round
+
         s21_truncate_big(tmp, &value_unsigned_truncated);
         s21_sub_big(tmp, value_unsigned_truncated, &fractional, 0);
         tmp = s21_round_banking_big(value_unsigned_truncated, fractional);
 
-        // s21_print_big_decimal_number(&tmp);
-        // }
-        // flg_div = 0;
         power_of_result--;
         rewrite = check_big_decimal(tmp);
       }
@@ -190,11 +182,6 @@ int s21_big_mul(s21_big_decimal big_value_1, s21_big_decimal big_value_2,
       } else {
         output = CONVERSATION_OK;
         *result = tmp;
-        // s21_print_big_decimal_number(result);
-        // if (power_of_1 || power_of_2) {  // было &&
-        //   s21_truncate_zero_big(result);
-        //   s21_print_big_decimal_number(result);
-        // }
       }
     } else {
       return (output = CONVERSATION_BIG);
@@ -210,46 +197,8 @@ int s21_big_mul(s21_big_decimal big_value_1, s21_big_decimal big_value_2,
   } else {
     *result = tmp;
   }
-  return output;  // при заполнении всего биг_децимал вернет 0
+  return output;
 }
-
-// int count_ones(s21_big_decimal v_1, s21_big_decimal v_2) {
-//   int count_sum = 0;
-//   int v1_95 = 95;
-//   int v2_95 = 95;
-//   while (!s21_get_bit_big(&v_1, v1_95)) {
-//     v1_95--;
-//   }
-//   while (!s21_get_bit_big(&v_2, v2_95)) {
-//     v2_95--;
-//   }
-//   count_sum = v1_95 + v2_95;
-//   if (count_sum >= 96)
-//     return (count_sum - 96);
-//   else
-//     return (0);
-// }
-
-// int count_ones(s21_big_decimal v_1, s21_big_decimal v_2) {
-//   int count_sum = 0;
-//   int count_1 = 0;
-//   int count_2 = 0;
-//   int v1_95 = 95;
-//   int v2_95 = 95;
-//   while (v1_95) {
-//     count_1 += s21_get_bit_big(&v_1, v1_95);
-//     v1_95--;
-//   }
-//   while (v2_95) {
-//     count_2 += s21_get_bit_big(&v_2, v2_95);
-//     v2_95--;
-//   }
-//   count_sum = count_1 + count_2;
-//   if (count_sum >= 96)
-//     return (count_sum - 96);
-//   else
-//     return (0);
-// }
 
 /*
     работает
--- s21_decimal.h
+++ s21_decimal.h
@@ -185,6 +185,5 @@ int s21_div_decimal_by_10_big(s21_big_decimal *value_1,
                               s21_big_decimal *out_reminder);
 
 int check_big_decimal(s21_big_decimal big_tmp);
-int count_ones(s21_big_decimal v_1, s21_big_decimal v_2);
 
 #endif  // _SRC_S21_DECIMAL_H_
